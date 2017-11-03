01c07b508cd1282f04349641c0e3cf8147c0c886
WillyScott <wgromme@yahoo.com>
2017-11-03 12:08:27 -0400

    RecyclerView working.

---
+++ app/src/main/java/com/example/dad/kotlincard/Card.kt
@@ -0,0 +1,9 @@
+package com.example.dad.kotlincard
+
+import java.io.Serializable
+
+
+/**
+ * Created by Dad on 11/2/2017.
+ */
+data class Card (var front: String?, var back: String?) :Serializable
---
+++ app/src/main/java/com/example/dad/kotlincard/CardFetcher.kt
@@ -0,0 +1,9 @@
+package com.example.dad.kotlincard
+
+/**
+ * Created by Dad on 11/3/2017.
+ */
+class CardFetcher {
+
+    
+}
