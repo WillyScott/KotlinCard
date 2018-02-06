29b9c8c402c5e02a70c278138f6a30040f580c49
WillyScott <wgromme@yahoo.com>
2018-02-06 14:57:40 -0500

    removed new lines from JSON, ...

--- cardapp/src/main/java/com/williamgromme/kotlincard/import/PreloadedData.kt
+++ cardapp/src/main/java/com/williamgromme/kotlincard/import/PreloadedData.kt
@@ -6,10 +6,10 @@ package com.williamgromme.kotlincard.import
 class PreloadedData {
 
     companion object {
-        val fileNames = arrayOf("kotlin_keywordsV1_1_60.json","introductionSwiftCard.json" )
+        val fileNames = arrayOf("kotlin_keywordsV1_1_60.json","introductionKotlinCard.json" )
         val setNames = arrayOf("Kotlin keywords","introduction Kotlin Card" )
         val setSection = arrayOf("kotlin_keywordsV1_1_60","Introduction to App" )
         val fileURLs = arrayOf("https://raw.githubusercontent.com/WillyScott/FlashCardsKotlin/master/kotlin_keywordsV1_1_60.json",
-                "https://raw.githubusercontent.com/WillyScott/FlashCardsData/master/introductionSwiftCard.json" )
+                "https://raw.githubusercontent.com/WillyScott/FlashCardsKotlin/master/introductionKotlinCard.json" )
     }
 }
